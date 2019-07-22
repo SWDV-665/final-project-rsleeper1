f5e6414b7336dea5d9fb019dee25cea1f5ee2c90
rsleeper1 <39035524+rsleeper1@users.noreply.github.com>
2019-07-22 00:06:49 -0500

    Puppy Pals Updates

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,98 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  title = "My Dogs"
+  myDogs = []
+
+
+  constructor(public alertController: AlertController, public toastController: ToastController, private camera: Camera) {}
+
+
+  addDog(){
+    this.presentAlertPrompt();
+  }
+
+  removeDog(dog, index){
+    this.presentToast(dog, index);
+  }
+    
+    async presentAlertPrompt() {
+      const alert = await this.alertController.create({
+        header: 'New Dog',
+        inputs: [
+          {
+            name: 'name',
+            type: 'text',
+            placeholder: 'Name'
+          },
+          {
+            name: 'breed',
+            type: 'text',
+            placeholder: 'Breed'
+          },
+          {
+            name: 'gender',
+            type: 'text',
+            placeholder: 'Male/Female'
+          },
+          {
+            name: 'size',
+            type: 'text',
+            placeholder: 'Small/Medium/Large'
+          },
+          {
+            name: 'age',
+            type: 'text',
+            placeholder: 'Age'
+          },
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+              console.log('Confirm Cancel');
+            }
+          }, {
+            text: 'Ok',
+            handler: (dog) => {
+              console.log('Confirm Ok', dog);
+              this.myDogs.push(dog);
+            }
+          }
+        ]
+      });
+  
+      await alert.present();
+    }
+
+  async presentToast(dog, index) {
+    const toast = await this.toastController.create({
+      message: 'Removing dog - ' + dog.name + "...",
+      duration: 2000
+    });
+    toast.present();
+
+    this.myDogs.splice(index, 1);
+  }
+  
+  loadImage(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+    // imageData is either a base64 encoded string or a file URI
+    // If it's base64 (DATA_URL):
+    let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+    // Handle error
+    });
+  }
+  
 
 }
